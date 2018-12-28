f0a67b2e3ac43790b877e02d45151855f42c34a7
William Keilsohn <kingwilliam42@gmail.com>
2018-12-27 20:23:40 -0500

    Count Down from x to y via z
    Given an range of two numbers, count down from the larger number to the smaller one via a specified increment.

---
+++ Count_Down.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 27 09:27:31 2018
+
+@author: William Keilsohn 
+"""
+
+'''
+Count down from a given number to another viaa certian interval.
+'''
+
+num = 800
+
+while num > 200:
+    num -= 100
+#    print(num) #Just checks that everything is going according to plan.
+    
+
+### Starting number can be altered as needed.
+### Deincrements can be adjusted.
+### Ending number is easy to change, and based on deincrements/specificity of the question it may be better to include a >= instead of > sign.
